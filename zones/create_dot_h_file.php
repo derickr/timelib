b51aee8b4a9bd29634338eceb50620108e96814a
Derick Rethans <github@derickrethans.nl>
2015-06-20 00:04:36 +0100

    Comment out setting of the ASCII values

--- zones/create_dot_h_file.php
+++ zones/create_dot_h_file.php
@@ -45,7 +45,7 @@ for ($i = 0; $i < $dta_l; $i++)
 	}
 	$char = ord( $dta[$i] );
 	$data .= sprintf( "0x%02X,", $char );
-	$ascii .= ($char >= 32 && $char != 47 && $char < 127) ? sprintf( "%c", $char ) : '.';
+	//$ascii .= ($char >= 32 && $char != 47 && $char < 127) ? sprintf( "%c", $char ) : '.';
 
 	if ( $j % 16 == 15 )
 	{
