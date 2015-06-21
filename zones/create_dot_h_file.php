2979f9e81cb783089e9a390ab581dfc34f2e8eb6
Derick Rethans <github@derickrethans.nl>
2015-06-21 22:55:43 +0100

    Instead of commenting this out, we're making it an option

--- zones/create_dot_h_file.php
+++ zones/create_dot_h_file.php
@@ -45,7 +45,11 @@ for ($i = 0; $i < $dta_l; $i++)
 	}
 	$char = ord( $dta[$i] );
 	$data .= sprintf( "0x%02X,", $char );
-	//$ascii .= ($char >= 32 && $char != 47 && $char < 127) ? sprintf( "%c", $char ) : '.';
+
+	if ( $argv[1] == 'debug' )
+	{
+		$ascii .= ($char >= 32 && $char != 47 && $char < 127) ? sprintf( "%c", $char ) : '.';
+	}
 
 	if ( $j % 16 == 15 )
 	{
