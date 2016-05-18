af5be49655b1911f4d8cd10489806a8190df1b69
Derick Rethans <github@derickrethans.nl>
2016-05-18 11:23:33 +0100

    Fixed PHP bug #64740: strtotime seems to use both sunday and monday as start of week

---
+++ tests/files/bug63470.ts
@@ -0,0 +1,13 @@
+Expected:      | String to parse:        | Hole filler:             | Timezone:              | Comments:
+TS: 1435536000 | this week               | 2015-07-04 00:00         | UTC
+TS: 1435536000 | this week               | 2015-07-05 00:00         | UTC                    | Sunday
+TS: 1436140800 | this week               | 2015-07-06 00:00         | UTC
+TS: 1436140800 | this week               | 2015-07-11 00:00         | UTC
+TS: 1436140800 | this week               | 2015-07-12 00:00         | UTC                    | Sunday
+TS: 1436745600 | this week               | 2015-07-13 00:00         | UTC
+
+TS: 1209254400 |                         | 2008-04-27 00:00         | UTC                    | Thursday
+TS: 1209254400 | this week sunday        | 2008-04-25 00:00         | UTC                    | Thursday
+
+TS: 1208822400 |                         | 2008-04-22 00:00         | UTC                    | Thursday
+TS: 1208822400 | this week tuesday       | 2008-04-25 00:00         | UTC                    | Thursday
