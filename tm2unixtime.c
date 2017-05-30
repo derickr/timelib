3106884a41c8da2622de9cc395852739cb964760
Derick Rethans <github@derickrethans.nl>
2017-05-30 11:55:22 +0100

    Shortcut date calculations

--- tm2unixtime.c
+++ tm2unixtime.c
@@ -202,6 +202,34 @@ void timelib_do_rel_normalize(timelib_time *base, timelib_rel_time *rt)
 	do_range_limit(0, 12, 12, &rt->m, &rt->y);
 }
 
+#define EPOCH_DAY 719468
+
+static void magic_date_calc(timelib_time *time)
+{
+	timelib_sll y, ddd, mi, mm, dd, g;
+
+	/* The algorithm doesn't work before the year 1 */
+	if (time->d < -719498) {
+		return;
+	}
+
+	g = time->d + EPOCH_DAY - 1;
+
+	y = (10000 * g + 14780) / 3652425;
+	ddd = g - ((365*y) + (y/4) - (y/100) + (y/400));
+	if (ddd < 0) {
+		y--;
+		ddd = g - ((365*y) + (y/4) - (y/100) + (y/400));
+	}
+	mi = (100 * ddd + 52) / 3060;
+	mm = ((mi + 2) % 12) + 1;
+	y = y + (mi + 2) / 12;
+	dd = ddd - ((mi * 306 + 5) / 10) + 1;
+	time->y = y;
+	time->m = mm;
+	time->d = dd;
+}
+
 void timelib_do_normalize(timelib_time* time)
 {
 	if (time->f != TIMELIB_UNSET) do_range_limit_fraction(&time->f, &time->s);
@@ -209,6 +237,11 @@ void timelib_do_normalize(timelib_time* time)
 	if (time->s != TIMELIB_UNSET) do_range_limit(0, 60, 60, &time->i, &time->h);
 	if (time->s != TIMELIB_UNSET) do_range_limit(0, 24, 24, &time->h, &time->d);
 	do_range_limit(1, 13, 12, &time->m, &time->y);
+
+	/* Short cut if we're doing things against the Epoch */
+	if (time->y == 1970 && time->m == 1 && time->d != 1) {
+		magic_date_calc(time);
+	}
 
 	do {} while (do_range_limit_days(&time->y, &time->m, &time->d));
 	do_range_limit(1, 13, 12, &time->m, &time->y);
