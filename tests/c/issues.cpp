2e7f20ee04e4211dc46910ec1c005dce4aa75c46
Derick Rethans <github@derickrethans.nl>
2017-08-30 19:37:43 +0100

    Added callgrind files to .gitignore, and fixed some WS issues

--- tests/c/issues.cpp
+++ tests/c/issues.cpp
@@ -15,7 +15,7 @@ TEST(issues, issue0017_test1)
 	LONGS_EQUAL(2369, t->y);
 	LONGS_EQUAL(12, t->m);
 	LONGS_EQUAL(30, t->d);
-	
+
 	timelib_time_dtor(t);
 }
 
@@ -29,7 +29,7 @@ TEST(issues, issue0017_test2)
 	LONGS_EQUAL(2369, t->y);
 	LONGS_EQUAL(12, t->m);
 	LONGS_EQUAL(31, t->d);
-	
+
 	timelib_time_dtor(t);
 }
 
@@ -43,7 +43,7 @@ TEST(issues, issue0017_test3)
 	LONGS_EQUAL(2370, t->y);
 	LONGS_EQUAL(1, t->m);
 	LONGS_EQUAL(1, t->d);
-	
+
 	timelib_time_dtor(t);
 }
 
