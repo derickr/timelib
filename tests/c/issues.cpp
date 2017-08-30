82677ac41926b36e77d03fd04081af0c1b9d0c2e
Derick Rethans <github@derickrethans.nl>
2017-08-30 19:36:37 +0100

    Fixed #19: Inefficient algorithm for calculating dates far in the past.
    Also related to PHP bug #75088.

--- tests/c/issues.cpp
+++ tests/c/issues.cpp
@@ -46,3 +46,87 @@ TEST(issues, issue0017_test3)
 	
 	timelib_time_dtor(t);
 }
+
+TEST(issues, issue0019_test1)
+{
+	timelib_sll   ts = -172800;
+	timelib_time *t  = timelib_time_ctor();
+
+	timelib_set_timezone_from_offset(t, 0);
+	timelib_unixtime2local(t, ts);
+	LONGS_EQUAL(1969, t->y);
+	LONGS_EQUAL(12, t->m);
+	LONGS_EQUAL(30, t->d);
+
+	timelib_time_dtor(t);
+}
+
+TEST(issues, issue0019_test2)
+{
+	timelib_sll   ts = -86400;
+	timelib_time *t  = timelib_time_ctor();
+
+	timelib_set_timezone_from_offset(t, 0);
+	timelib_unixtime2local(t, ts);
+	LONGS_EQUAL(1969, t->y);
+	LONGS_EQUAL(12, t->m);
+	LONGS_EQUAL(31, t->d);
+
+	timelib_time_dtor(t);
+}
+
+TEST(issues, issue0019_test3)
+{
+	timelib_sll   ts = 0;
+	timelib_time *t  = timelib_time_ctor();
+
+	timelib_set_timezone_from_offset(t, 0);
+	timelib_unixtime2local(t, ts);
+	LONGS_EQUAL(1970, t->y);
+	LONGS_EQUAL(1, t->m);
+	LONGS_EQUAL(1, t->d);
+
+	timelib_time_dtor(t);
+}
+
+TEST(issues, issue0019_test4)
+{
+	timelib_sll   ts = -12622953600;
+	timelib_time *t  = timelib_time_ctor();
+
+	timelib_set_timezone_from_offset(t, 0);
+	timelib_unixtime2local(t, ts);
+	LONGS_EQUAL(1569, t->y);
+	LONGS_EQUAL(12, t->m);
+	LONGS_EQUAL(30, t->d);
+
+	timelib_time_dtor(t);
+}
+
+TEST(issues, issue0019_test5)
+{
+	timelib_sll   ts = -12622867200;
+	timelib_time *t  = timelib_time_ctor();
+
+	timelib_set_timezone_from_offset(t, 0);
+	timelib_unixtime2local(t, ts);
+	LONGS_EQUAL(1569, t->y);
+	LONGS_EQUAL(12, t->m);
+	LONGS_EQUAL(31, t->d);
+
+	timelib_time_dtor(t);
+}
+
+TEST(issues, issue0019_test6)
+{
+	timelib_sll   ts = -12622780800;
+	timelib_time *t  = timelib_time_ctor();
+
+	timelib_set_timezone_from_offset(t, 0);
+	timelib_unixtime2local(t, ts);
+	LONGS_EQUAL(1570, t->y);
+	LONGS_EQUAL(1, t->m);
+	LONGS_EQUAL(1, t->d);
+
+	timelib_time_dtor(t);
+}
