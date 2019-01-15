98ab28790fa1e9d42b794f790e63b7902e55e8b4
Derick Rethans <github@derickrethans.nl>
2019-01-15 11:36:03 +0000

    Sort out copyright headers

--- timelib.h
+++ timelib.h
@@ -2,6 +2,7 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2015-2019 Derick Rethans
+ * Copyright (c) 2018 MongoDB, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
