ba7eb0773cd321302ef5580030389c578cccd00a
Derick Rethans <github@derickrethans.nl>
2026-08-06 16:55:38 +0100

    Go with 2026.02

--- timelib.h
+++ timelib.h
@@ -30,9 +30,9 @@
 # include "timelib_config.h"
 #endif
 
-#define TIMELIB_VERSION 202601
-#define TIMELIB_EXTENDED_VERSION 20260101
-#define TIMELIB_ASCII_VERSION "2026.01"
+#define TIMELIB_VERSION 202602
+#define TIMELIB_EXTENDED_VERSION 20260201
+#define TIMELIB_ASCII_VERSION "2026.02"
 
 #include <stdlib.h>
 #include <stdbool.h>
